4b239405d8221d78a1a96df55884a244702b462f
KeitaAkiyama <mkeita68@gmail.com>
2017-12-11 11:59:35 +0100

    select fonctionne + un peu css

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -8,10 +8,11 @@ import { RemoteSqlProvider } from '../../providers/remotesql/remotesql';
 })
 
 export class HelloIonicPage {
-    public replique: Array<{ profid: number, libelle: string, nom: string, pre: string, date: string }>
+    public replique: Array<{ profid: number, libelle: string, nom: string, pre: string }>
+    public repliquetemp: Array<{ profid: number, libelle: string, nom: string, pre: string }>
     public selectprof: Array<{ id: number, nom: string, pre: string }> 
-    public reqprof: Array<{profid: number, libelle: string, nom: string, pre: string, date: string}>
-
+    public reqprof: Array<{id : number,profid: number, libelle: string, nom: string, pre: string, date: string}>
+    public unProf: number;
     public change_valeur() {
     
     
@@ -23,14 +24,10 @@ export class HelloIonicPage {
         this.replique = [];
         this.selectprof = [];
         this.reqprof = [];
-        this.sqlPrd.select("SELECT nom,pre FROM prof", null, this.selectprof);
+        this.sqlPrd.select("SELECT id,nom,pre FROM prof", null, this.selectprof);
+        this.repliquetemp = [];
+        this.unProf=0;
         }
-
-   
-    
-	
-   
-
 
     btnreq() {
         this.replique = [];
@@ -39,12 +36,27 @@ export class HelloIonicPage {
     }
     
     btnprof() {
-        
-        var selectElmt = document.getElementById("select");
-        
-        this.reqprof = [];
-        this.sqlPrd.select("SELECT libelle,nom,pre FROM quotes AS q1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM quotes)) AS id) AS q2  INNER JOIN prof ON q1.idprof = prof.id WHERE  q1.id >= q2.id AND q1.idprof = 2 ORDER BY q1.id ASC LIMIT 1 ", null, this.reqprof); 
+        document.getElementById("random")
+        this.repliquetemp =[];
+        this.sqlPrd.select("SELECT idprof,libelle,nom,pre FROM quotes,prof WHERE quotes.idprof = prof.id AND quotes.idprof =? ORDER BY quotes.id ASC", [this.unProf], this.repliquetemp).then((data)=>
+        {
+        var taille = this.repliquetemp.length;
+        console.log(taille);
+        if(this.unProf!=0)
+        { 
+            if(this.repliquetemp.length>1){
+            this.reqprof = [];
+            this.sqlPrd.select("SELECT libelle,nom,pre FROM quotes AS q1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM quotes)) AS id) AS q2  INNER JOIN prof ON q1.idprof = prof.id WHERE  q1.id >= q2.id AND q1.idprof =? ORDER BY q1.id ASC LIMIT 1 ", [this.unProf], this.reqprof);     
 
+            }
+            else
+            {
+            this.reqprof=[];
+            this.sqlPrd.select("SELECT libelle,nom,pre FROM quotes,prof prof WHERE quotes.idprof = prof.id AND quotes.idprof =? ORDER BY quotes.id ASC Limit 1", [this.unProf], this.reqprof); 
+            }
+        }
+
+        });    
     }
         
         
